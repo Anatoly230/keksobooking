71c638fb6658657ce2212bc1d31ca71d18d4fd83
Anatoly230 <maximos225@mail.ru>
2022-10-20 14:30:55 +0300

    Дов=авил функцию для добавления префикса

---
+++ js/main.js
@@ -0,0 +1,67 @@
+function getValueOfArguments(from, to) {
+    if (typeof from !== "number") {
+        return false;
+    }
+    if (from < 0) {
+        from = Math.abs(from);
+    }
+    if (from > to) {
+        [from, to] = [to, from];
+    }
+
+    return {
+        from: from,
+        to: to
+    }
+}
+
+function getRandomNum(from = 1000, to = 0) {
+    try {
+        let range = getValueOfArguments(from, to);
+        from = range.from,
+            to = range.to;
+        range = null;
+        return Math.floor(Math.random() * (to - from + 1)) + from;
+    } catch (err) {
+        console.log(err)
+        return false;
+    }
+
+}
+
+function getRandomFloat(from = 1000, to = 0, countNum = 3) {
+    try {
+        let range = getValueOfArguments(from, to);
+        from = range.from,
+            to = range.to;
+        range = null;
+        return Number((Math.random() * (to - from + 1) + from).toFixed(countNum));
+    } catch (err) {
+        console.log(err.stack);
+        return false;
+    }
+}
+
+
+function defineStringLength(str, charCount) {
+    if (str.length > charCount) {
+        return false;
+    }
+    return true;
+}
+
+function addPrefix(num, prefix = 0) {
+    try {
+        if (num === undefined || typeof num === "object") {
+            throw new Error("Ошибка ввода, неоходимо ввести число либо строку")
+        }
+        if (num < 10) {
+            return prefix + "" + num
+        }
+        return num + "";
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+console.log("код в норме")
