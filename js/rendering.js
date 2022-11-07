a387df52aef595134eab4dedbe18093883d9839a
Anatoly230 <maximos225@mail.ru>
2022-11-07 19:43:27 +0300

    Неверная линия изменений

--- js/rendering.js
+++ js/rendering.js
@@ -1,5 +1,5 @@
-import {getAddInfo} from "./data.js";
-import {getRandomNum} from "./utils.js";
+import { getAddInfo } from "./data.js";
+import { getRandomNum } from "./utils.js";
 
 const template = document.querySelector("#card").content.querySelector(".popup");
 const home = document.querySelector("#map-canvas");
@@ -19,6 +19,22 @@ console.log("Будущий дочерний элемент", child);
 console.log("price", child.querySelector(".popup__text--price"))
 console.log(home)
 console.log(rawData)
+console.log(rawData.offer.features)
+
+
+
+Array.from(child.querySelector(".popup__features").children).forEach(function (item, index) {
+    item.classList.forEach(function (subItem) {
+        rawData.offer.features.forEach(function (dataItem) {
+            if (!(subItem.includes(`--${dataItem}`))) {
+                console.log(child.querySelector(".popup__features").children[index])
+            }
+        })
+    })
+})
+// child.features.forEach(function(item){
+// console.log(item)
+// })
 
 home.appendChild(child);
 
