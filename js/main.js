ecd38da2392a66a178bee1cf2098664c14ca45be
Anatoly <maximos225@mail.ru>
2023-03-15 18:55:23 +0300

    начал заниматься картой

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ import { renderData } from "./rendering.js"
 import { activateInputs, deactivateInputs, buch } from "./activate.js"
 import { pristine } from "./validate.js"
 import { slider } from "./slider-generator.js"
+import { map } from "./map.js"
 
 console.log("код в норме")
 
-renderData();
+// renderData();
 
 
 
---
+++ js/map.js
@@ -0,0 +1,10 @@
+const map = L.map('map-canvas')
+    .setView({
+        lat: 59.9274,
+        lng: 30.31127,
+    }, 10)
+
+
+console.log(map);
+
+export { map };
